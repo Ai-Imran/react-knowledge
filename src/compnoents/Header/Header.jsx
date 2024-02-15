70eb5e56c8f7adb8c8aedb0868497484ca12711f
Ai-Imran <ikimranahmed004@gamil.com>
2024-02-15 17:23:24 +0600

    first commit

---
+++ src/compnoents/Header/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-4xl text-center font-extrabold mt-3 text-purple-600'>Konwlkedge Cafe</h1>
+        </div>
+    );
+};
+
+export default Header;
